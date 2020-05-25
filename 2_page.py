9387639ed44bab9c652f2129bbca6cc4ba0595bb
barzgara71961 <51098311+barzgara71961@users.noreply.github.com>
2020-05-26 10:56:08 +1200

    cahnges
    i have made some change but i dont reall like it

--- 2_page.py
+++ 2_page.py
@@ -32,18 +32,6 @@ class Quiz:
         self.button_lable_frame = Frame(self.quiz_frame,bg=background_color)
         self.button_lable_frame.grid(row=1)
 
-        # math_button button (row 0 column 1 )
-        self.math_button = Button(self.button_lable_frame,
-                                  text="Math level 1", font="arial 20 bold", fg="#757761",
-                                  bg="#51BBFE",command=self.math)
-        self.math_button.grid(row=0)
-
-        self.math_label = Label(self.button_lable_frame,
-                                text="Math level 1 will give will give you a quiz\n"
-                                     "about addtion and subcration",
-                                font="arial 10 bold", fg="#757761",bg="#F7FE72")
-        self.math_label.grid(row=0,column=1)
-
         self.geo_button = Button(self.button_lable_frame,
                                   text="Geography", font="arial 20 bold", fg="#757761",
                                   bg="#51BBFE",padx=5,command=self.geo)
@@ -53,17 +41,6 @@ class Quiz:
                                 text="This will give you a test about capital",
                                 font="arial 10 bold", fg="#757761",bg="#F7FE72")
         self.geo_label.grid(row=2,column=1)
-
-        self.math_three_button = Button(self.button_lable_frame,
-                                  text="Math level 3", font="arial 20 bold", fg="#757761",
-                                  bg="#51BBFE")
-        self.math_three_button.grid(row=3)
-
-        self.math_label = Label(self.button_lable_frame,
-                                text="Math level 3 will give will give you a quiz\n"
-                                     "about i dont what will go here ",
-                                font="arial 10 bold", fg="#757761",bg="#F7FE72")
-        self.math_label.grid(row=3,column=1)
 
         self.help_frame = Frame(self.quiz_frame)
         self.help_frame.grid(row=2)
@@ -79,82 +56,58 @@ class Quiz:
         get_help = Help(self)
         get_help.help_text.configure(text="You have to pick a top and you will be getting tested on in")
 
-    def math(self):
-        math_L1 = Math(self)
-        math_L1.math_text.configure(text="Fill the boxes")
-
     def geo(self):
         geo = Geo(self)
         geo.geo_text.configure(text="Fill the boxes")
-
-class Math:
-    def __init__(self, partner):
-        background_color = "#8FF7A7"
-
-        # disable button
-        partner.math_button.config(state=DISABLED)
-
-        # Set up math game one
-        self.math_box = Toplevel()
-
-        # Set up GUI Frame
-        self.math_frame = Frame(self.math_box, width=300, bg=background_color)
-        self.math_frame.grid()
-        # Set up math Instruction heading (row 0)
-        self.how_heading = Label(self.math_frame,
-                                 text="Math",
-                                 font="arial 20 bold",bg=background_color)
-        self.how_heading.grid(row=0)
-        # Math text (label, row 1)
-        self.math_text = Label(self.math_frame,
-                               text="Fill the boxes",
-                               justify=LEFT,width=50, bg=background_color,wrap=200)
-        self.math_text.grid(column=0,row=1)
-
-        # Dismiss button (row 2)
-        self.dismiss_btn = Button(self.math_frame,text="Dismiss",width=10,bg="red",
-                                  font="arial 10 bold",
-                                  command=partial(self.close_math, partner))
-        self.dismiss_btn.grid(row=2, pady=10)
-
-    def close_math(self, partner):
-        # Put help button back to normal
-        partner.math_button.config(state=NORMAL)
-        self.math_box.destroy()
 
 class Geo:
     def __init__(self, partner):
         background_color = "#8FF7A7"
 
         # disable button
-        partner.math_button.config(state=DISABLED)
+        partner.geo_button.config(state=DISABLED)
 
         # Set up Geo game one
         self.geo_box = Toplevel()
-
         # Set up GUI Frame
-        self.geo_frame = Frame(self.geo_box, width=300, bg=background_color)
-        self.geo_frame.grid()
+        self.heading_frame = Frame(self.geo_box, width=300, bg=background_color)
+        self.heading_frame.grid(row=1)
         # Set up Geo Instruction heading (row 0)
-        self.how_heading = Label(self.geo_frame,
+        self.how_heading = Label(self.heading_frame,
                                  text="Geo",
                                  font="arial 20 bold",bg=background_color)
         self.how_heading.grid(row=0)
         # Geo text (label, row 1)
-        self.geo_text = Label(self.geo_frame,
+        self.geo_text = Label(self.heading_frame,
                                text="Fill the boxes",
                                justify=LEFT,width=50, bg=background_color,wrap=200)
         self.geo_text.grid(column=0,row=1)
 
+        self.cho_btn__frame = Frame(self.geo_box, width=300, bg=background_color)
+        self.cho_btn__frame.grid(row=2)
+
+        self.capital_btn = Button(self.cho_btn__frame,text="Capitals",width=10,bg="#51BBFE",
+                                  font="arial 20 bold")
+        self.capital_btn.grid(row=1,column=1)
+
+        self.countries_btn = Button(self.cho_btn__frame,text="Countries ",width=10,bg="#51BBFE",
+                                  font="arial 20 bold")
+        self.countries_btn.grid(row=1,column=2)
+
+
+        # Set up GUI Frame
+        self.diss_btn__frame = Frame(self.geo_box, width=300, bg=background_color)
+        self.diss_btn__frame.grid(row=3)
+
         # Dismiss button (row 2)
-        self.dismiss_btn = Button(self.geo_frame,text="Dismiss",width=10,bg="red",
+        self.dismiss_btn = Button(self.diss_btn__frame,text="Dismiss",width=10,bg="red",
                                   font="arial 10 bold",
                                   command=partial(self.close_geo, partner))
-        self.dismiss_btn.grid(row=2, pady=10)
+        self.dismiss_btn.grid(row=2)
 
     def close_geo(self, partner):
         # Put help button back to normal
-        partner.math_button.config(state=NORMAL)
+        partner.geo_button.config(state=NORMAL)
         self.geo_box.destroy()
 
 class Help:
@@ -162,7 +115,7 @@ class Help:
         background_color = "#8FF7A7"
 
         # disable help button
-        partner.help_button.config(state=DISABLED)
+        partner.geo_button.config(state=DISABLED)
 
         # Set up child window (ie: help box)
         self.help_box = Toplevel()
@@ -189,7 +142,7 @@ class Help:
 
     def close_help(self, partner):
         # Put help button back to normal
-        partner.help_button.config(state=NORMAL)
+        partner.geo_button.config(state=NORMAL)
         self.help_box.destroy()
 
 # main routine
